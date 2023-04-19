21c26fc46ee50d979d6b9536ef0dfdbc5ab385c7
Danialprabukj <danialprabu03@gmail.com>
2023-04-19 23:00:56 +0530

    first commit

---
+++ src/main/java/com/kgisl/upload/exception/FileUploadExceptionAdvice.java
@@ -0,0 +1,23 @@
+package com.kgisl.upload.exception;
+
+
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.kgisl.upload.message.ResponseMessage;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(MaxUploadSizeExceededException.class)
+  public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+    return ResponseEntity
+        .status(HttpStatus.EXPECTATION_FAILED)
+        .body(new ResponseMessage("One or more files are too large!"));
+  }
+}
